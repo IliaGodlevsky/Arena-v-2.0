063de6627c0ac78fd559eef91aebb5492d992c87
IliaGodlevsky <godleevsky2015@gmail.com>
2019-12-21 11:20:42 +0300

    Update DefenceItemFactory.cpp

--- Factories/ItemFactory/DefenceItemFactory.cpp
+++ Factories/ItemFactory/DefenceItemFactory.cpp
@@ -8,14 +8,18 @@
 #include "../ArmorFactories/LegionerChainsFactory.h"
 #include "../ArmorFactories/ArmorFactory.h"
 #include "../MagicFactories/AttackFactories/AttackMagicFactory.h"
+#include "../WeaponFactories/MagicSwordFactory.h"
+#include "../WeaponFactories/WeaponFactory.h"
 
 DefenceItemFactory::DefenceItemFactory()
 {
-	m_magicFactories.push_back(new DamageDebuffFactory());
-	m_magicFactories.push_back(new SilenceFactory());
+	//m_magicFactories.push_back(new DamageDebuffFactory());
+	//m_magicFactories.push_back(new SilenceFactory());
 	m_magicFactories.push_back(new AttackAndStunMagicFactory());
 	m_magicFactories.push_back(new AttackMagicFactory());
 	m_shieldFactories.push_back(new ShieldFactory());
 	m_armorFactories.push_back(new LegionerChainsFactory());
 	m_armorFactories.push_back(new ArmorFactory());
+	m_weaponFactories.push_back(new MagicSwordFactory());
+	m_weaponFactories.push_back(new WeaponFactory());
 }
