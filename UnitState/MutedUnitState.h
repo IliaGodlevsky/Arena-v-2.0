6393746c101cd209ae6cbaa60d7772eeda7f0d41
IliaGodlevsky <godleevsky2015@gmail.com>
2019-12-22 10:03:32 +0300

    Update MutedUnitState.h

--- UnitState/MutedUnitState.h
+++ UnitState/MutedUnitState.h
@@ -7,8 +7,10 @@ class MutedUnitState : public UnitState
 {
 public:
 	MutedUnitState(int duration);
-	UnitPtr chooseUnitToAttack(const Unit& decidingUnit, const Gladiators& units)const override;
-	MagicPtr chooseMagicToCast(const Unit& decidingUnit, const Gladiators& units)const override;
+	UnitPtr chooseUnitToAttack(const Unit& decidingUnit, 
+		const Gladiators& units)const override;
+	MagicPtr chooseMagicToCast(const Unit& decidingUnit, 
+		const Gladiators& units)const override;
 	UnitPtr chooseUnitToCast(const Unit& decidingUnit,
 		const MagicPtr& magicToCast, const Gladiators& units)const override;
 private:
