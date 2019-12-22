b8fffa84a6df90323c014453fc192dfd8b32798b
IliaGodlevsky <godleevsky2015@gmail.com>
2019-12-22 11:41:14 +0300

    Update UnitFactory.cpp

--- Factories/UnitFactory/UnitFactory.cpp
+++ Factories/UnitFactory/UnitFactory.cpp
@@ -19,17 +19,11 @@ UnitFactory::UnitFactory():
 UnitPtr UnitFactory::createUnit()const
 {
 	index factoryNumber, decisionNumber;
-	std::cout << m_itemFactories.size() << std::endl;
-	system("pause");
 	factoryNumber = inputNumber("Choose item factory\n1. Defence 2. Offence\n",
 		m_itemFactories.size(), 1);
 	decisionNumber = inputNumber("Choose decision\n1. Human 2. Computer", 
 		m_decisions.size(), 1);
 	std::string randomName = m_unitsNames[randomNumber(m_unitsNames.size() - 1)];
-	return UnitPtr(new Unit
-	(
-		randomName, 
-		m_decisions[decisionNumber - 1], 
-		m_itemFactories[factoryNumber - 1])
-	);
+	return UnitPtr(new Unit(randomName, m_decisions[decisionNumber - 1], 
+		m_itemFactories[factoryNumber - 1]));
 }
