3e1198c6013b229aef63f29504ee1aebc9143611
IliaGodlevsky <godleevsky2015@gmail.com>
2019-12-24 17:40:39 +0300

    added hread to load names

--- Factories/UnitFactory/UnitFactory.cpp
+++ Factories/UnitFactory/UnitFactory.cpp
@@ -5,9 +5,11 @@
 
 UnitFactory::UnitFactory():
 	m_decisions(0),
-	m_unitsNames(loadFromFile("Names.txt")),
 	m_itemFactories(0)
 {
+	// downloading names from file in the new thread
+	m_thread = std::unique_ptr<std::thread>(new std::thread([&]()
+	{ m_unitsNames = loadFromFile("Names.txt"); }));
 	m_decisions.push_back(DecisionPtr(new HumanDecision()));
 	m_itemFactories.push_back(ItemFactoryPtr(new DefenceItemFactory()));
 	m_itemFactories.push_back(ItemFactoryPtr(new OffenceItemFactory()));
@@ -20,6 +22,13 @@ UnitPtr UnitFactory::createUnit()const
 		"Choose item factory: ", m_itemFactories.size(), 1);
 	decisionNumber = inputNumber("1. Human 2. Computer\n"
 		"Choose decision: ", m_decisions.size(), 1);
+	// stops the thread and deletes it
+	if (m_thread->joinable())
+	{
+		m_thread->join();
+		m_thread->~thread();
+		m_thread.~unique_ptr();
+	}
 	std::string randomName = m_unitsNames[randomNumber(m_unitsNames.size() - 1)];
 	return UnitPtr(new Unit(randomName, m_decisions[decisionNumber - 1], 
 		m_itemFactories[factoryNumber - 1]));
--- Factories/UnitFactory/UnitFactory.h
+++ Factories/UnitFactory/UnitFactory.h
@@ -1,6 +1,8 @@
 #ifndef UNIT_FACTORY_H_
 #define UNIT_FACTORY_H_
 
+#include <thread>
+
 #include "../../Unit/Unit.h"
 #include "../Factory.h"
 #include "../ItemFactory/ItemFactory.h"
@@ -14,7 +16,9 @@ public:
 	UnitFactory();
 	UnitPtr createUnit()const;
 private:
-	mutable std::vector<std::string> m_unitsNames;
+	// a new thread for downloading names from file
+	mutable std::unique_ptr<std::thread> m_thread;
+	std::vector<std::string> m_unitsNames;
 	std::vector<ItemFactoryPtr> m_itemFactories;
 	std::vector<DecisionPtr> m_decisions;
 };
