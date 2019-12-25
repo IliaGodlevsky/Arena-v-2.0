097794384b0e9cd738700d6f320541bef44ebd6f
IliaGodlevsky <godleevsky2015@gmail.com>
2019-12-25 11:48:42 +0300

    changed checking the thread for being joinable

--- Factories/UnitFactory/UnitFactory.cpp
+++ Factories/UnitFactory/UnitFactory.cpp
@@ -7,7 +7,6 @@ UnitFactory::UnitFactory():
 	m_decisions(0),
 	m_itemFactories(0)
 {
-	// downloading names from file in the new thread
 	m_thread = std::unique_ptr<std::thread>(new std::thread([&]()
 	{ m_unitsNames = loadFromFile("Names.txt"); }));
 	m_decisions.push_back(DecisionPtr(new HumanDecision()));
@@ -22,13 +21,8 @@ UnitPtr UnitFactory::createUnit()const
 		"Choose item factory: ", m_itemFactories.size(), 1);
 	decisionNumber = inputNumber("1. Human 2. Computer\n"
 		"Choose decision: ", m_decisions.size(), 1);
-	// stops the thread and deletes it
 	if (m_thread->joinable())
-	{
 		m_thread->join();
-		m_thread->~thread();
-		m_thread.~unique_ptr();
-	}
 	std::string randomName = m_unitsNames[randomNumber(m_unitsNames.size() - 1)];
 	return UnitPtr(new Unit(randomName, m_decisions[decisionNumber - 1], 
 		m_itemFactories[factoryNumber - 1]));
