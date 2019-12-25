079f55dc01271e6f283af476175f43bdd7eea73b
IliaGodlevsky <godleevsky2015@gmail.com>
2019-12-25 11:51:59 +0300

    Update ActiveUnitState.h

--- UnitState/ActiveUnitState.h
+++ UnitState/ActiveUnitState.h
@@ -7,8 +7,10 @@ class ActiveUnitState : public UnitState
 {
 public:
 	ActiveUnitState();
-	UnitPtr chooseUnitToAttack(const Unit& decidingUnit, const Gladiators& units)const override;
-	MagicPtr chooseMagicToCast(const Unit& decidingUnit, const Gladiators& units)const override;
+	UnitPtr chooseUnitToAttack(const Unit& decidingUnit, 
+		const Gladiators& units)const override;
+	MagicPtr chooseMagicToCast(const Unit& decidingUnit, 
+		const Gladiators& units)const override;
 	UnitPtr chooseUnitToCast(const Unit& decidingUnit,
 		const MagicPtr& magicToCast, const Gladiators& units)const override;
 private:
