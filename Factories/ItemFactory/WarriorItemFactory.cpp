18e3cdd1d6bc2629f1132fe7485d0fb6f3722cd5
IliaGodlevsky <godleevsky2015@gmail.com>
2019-12-28 20:40:53 +0300

    added Warrior Factory

---
+++ Factories/ItemFactory/WarriorItemFactory.cpp
@@ -0,0 +1,28 @@
+#include "WarriorItemFactory.h"
+
+#include "../ArmorFactories/MailFactory.h"
+#include "../ArmorFactories/ArmorFactory.h"
+#include "../WeaponFactories/MagicSwordFactory.h"
+#include "../WeaponFactories/WeaponFactory.h"
+#include "../MagicFactories/BuffFactories/DamageBuffFactory.h"
+#include "../MagicFactories/BuffFactories/ArmorAndDamageBuffFactory.h"
+#include "../MagicFactories/BuffFactories/OffsetDamageBuffFactory.h"
+#include "../MagicFactories/BuffFactories/ArmorBuffFactory.h"
+#include "../MagicFactories/AttackFactories/PoisonMagicFactory.h"
+#include "../MagicFactories/AttackFactories/PoisonAndAttackMagicFactory.h"
+#include "../ShieldFactories/ShieldFactory.h"
+
+WarriorItemFactory::WarriorItemFactory()
+{
+	m_armorFactories.push_back(new MailFactory());
+	m_armorFactories.push_back(new ArmorFactory());
+	m_weaponFactories.push_back(new WeaponFactory());
+	m_weaponFactories.push_back(new MagicSwordFactory());
+	m_magicFactories.push_back(new OffsetDamageBuffFactory());
+	m_magicFactories.push_back(new ArmorAndDamageBuffFactory());
+	m_magicFactories.push_back(new ArmorBuffFactory());
+	m_magicFactories.push_back(new DamageBuffFactory());
+	m_magicFactories.push_back(new PoisonMagicFactory());
+	m_magicFactories.push_back(new PoisonAndAttackMagicFactory());
+	m_shieldFactories.push_back(new ShieldFactory());
+}
---
+++ Factories/ItemFactory/WarriorItemFactory.h
@@ -0,0 +1,12 @@
+#ifndef WARRIOR_ITEM_FACTORY_H_
+#define WARRIOR_ITEM_FACTORY_H_
+
+#include "ItemFactory.h"
+
+class WarriorItemFactory : public ItemFactory
+{
+public:
+	WarriorItemFactory();
+};
+
+#endif
