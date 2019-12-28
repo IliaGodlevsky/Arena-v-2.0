d89bb8172e237d5fd93d8b51155adde1eb7a5297
IliaGodlevsky <godleevsky2015@gmail.com>
2019-12-28 20:45:47 +0300

    added Staff class

---
+++ Weapon/Staff.cpp
@@ -0,0 +1,28 @@
+#include "Staff.h"
+
+#include "../Unit/Unit.h"
+
+Staff::Staff(std::string name, int damage)
+	: Weapon(name, damage, ZERO_CRITIAL_PROBABILITY)
+{
+
+}
+
+Staff::Staff(const Staff& weapon)
+	: Weapon(weapon)
+{
+
+}
+
+Staff& Staff::operator=(const Staff& weapon)
+{
+	if (this == &weapon)
+		return *this;
+	Weapon::operator=(weapon);
+	return *this;
+}
+
+void Staff::injureUnit(Unit& unit, int damage)const
+{
+	unit.m_health = unit.m_health - (m_damage + damage);
+}
---
+++ Weapon/Staff.h
@@ -0,0 +1,16 @@
+#ifndef STAFF_H_
+#define STAFF_H_
+
+#include "Weapon.h"
+
+class Staff : public Weapon
+{
+public:
+	Staff(std::string name, int damage);
+	Staff(const Staff& weapon);
+	Staff& operator=(const Staff& weapon);
+public:
+	virtual void injureUnit(Unit& unit, int damage)const;
+};
+
+#endif
