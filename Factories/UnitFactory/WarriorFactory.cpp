ba3521990a360ca14a12575cfa456e482936ceda
IliaGodlevsky <godleevsky2015@gmail.com>
2019-12-28 20:42:02 +0300

    changed items factories

--- Factories/UnitFactory/WarriorFactory.cpp
+++ Factories/UnitFactory/WarriorFactory.cpp
@@ -1,21 +1,16 @@
 #include "WarriorFactory.h"
 
+#include "../ItemFactory/WarriorItemFactory.h"
+
 WarriorFactory::WarriorFactory()
 	: UnitFactory()
 {
-
+	m_itemFactory = ItemFactoryPtr(new WarriorItemFactory());
 }
 
 UnitPtr WarriorFactory::createUnit()const
 {
-	index factoryNumber, decisionNumber;
-	factoryNumber = inputNumber("1. Defence 2. Offence\n"
-		"Choose item factory: ", m_itemFactories.size(), 1);
-	decisionNumber = inputNumber("1. Human 2. RandomComputer\n3.SimpleComputer\n"
-		"Choose decision: ", m_decisions.size(), 1);
-	if (m_thread->joinable())
-		m_thread->join();
+	checkThread();
 	std::string randomName = m_unitsNames[randomNumber(m_unitsNames.size() - 1)];
-	return UnitPtr(new Warrior(randomName, m_decisions[decisionNumber - 1],
-		m_itemFactories[factoryNumber - 1]));
+	return UnitPtr(new Warrior(randomName, m_decisions[chooseDecsion() - 1], m_itemFactory));
 }
--- Factories/UnitFactory/WizardFactory.cpp
+++ Factories/UnitFactory/WizardFactory.cpp
@@ -3,27 +3,23 @@
 #include "../../Unit/Wizard.h"
 #include "../../Factories/MagicFactories/AttackFactories/AttackAndStunMagicFactory.h"
 #include "../../Factories/MagicFactories/AttackFactories/AttackMagicFactory.h"
+#include "../ItemFactory/WizardItemFactory.h"
 
 WizardFactory::WizardFactory()
 	: UnitFactory()
 {
 	m_secondMagics.push_back(new AttackAndStunMagicFactory());
 	m_secondMagics.push_back(new AttackMagicFactory());
+	m_itemFactory = ItemFactoryPtr(new WizardItemFactory());
 }
 
 UnitPtr WizardFactory::createUnit()const
 {
-	index factoryNumber, decisionNumber, magicFactoryNumber;
-	factoryNumber = inputNumber("1. Defence 2. Offence\n"
-		"Choose item factory: ", m_itemFactories.size(), 1);
-	decisionNumber = inputNumber("1. Human 2. RandomComputer\n3.SimpleComputer\n"
-		"Choose decision: ", m_decisions.size(), 1);
-	if (m_thread->joinable())
-		m_thread->join();
-	magicFactoryNumber = randomNumber(m_secondMagics.size() - 1);
+	checkThread();
+	index magicFactoryNumber = randomNumber(m_secondMagics.size() - 1);
 	std::string randomName = m_unitsNames[randomNumber(m_unitsNames.size() - 1)];
-	return UnitPtr(new Wizard(randomName, m_decisions[decisionNumber - 1],
-		m_itemFactories[factoryNumber - 1], m_secondMagics[magicFactoryNumber]));
+	return UnitPtr(new Wizard(randomName, m_decisions[chooseDecsion() - 1],
+		m_itemFactory, m_secondMagics[magicFactoryNumber]));
 }
 
 WizardFactory::~WizardFactory()
