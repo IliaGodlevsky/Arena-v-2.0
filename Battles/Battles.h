7481b4ef9ad80bc752912d8ab633c07db305a2fa
IliaGodlevsky <godleevsky2015@gmail.com>
2020-01-02 10:47:13 +0300

    Update Battles.h

--- Battles/Battles.h
+++ Battles/Battles.h
@@ -1,11 +1,18 @@
 #ifndef BATTLES_H_
 #define BATTLES_H_
 
+/* A class that responses for damage and armor 
+of unit, weapon, shield and armor */
 class Battles
 {
 public:
 	explicit Battles(int value);
+	Battles(const Battles&) = default;
+	Battles(Battles&&) = default;
+	Battles& operator=(const Battles&) = default;
+	Battles& operator=(Battles&&) = default;
 	Battles() = default;
+public:
 	void changeValue(int value);
 	operator int()const;
 protected:
