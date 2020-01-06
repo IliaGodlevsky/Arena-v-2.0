8fe8749847a3f8c1a73effb06779e53ca160cfcf
IliaGodlevsky <godleevsky2015@gmail.com>
2020-01-06 19:06:44 +0300

    Update WizardLevel.cpp

--- Level/WizardLevel.cpp
+++ Level/WizardLevel.cpp
@@ -13,7 +13,6 @@ WizardLevel& WizardLevel::operator++()
 	m_unit->m_health = m_unit->m_health + 
 		Vitals(HP_PER_LVL, HP_PER_LVL, HP_REGEN_PER_LVL);
 	m_unit->m_health.restore(HP_RESTORE_PERCENT);
-
 	m_unit->m_mana.restore(MP_RESTORE_PERSENT);
 	m_unit->m_mana = m_unit->m_mana + 
 		Vitals(MP_PER_LVL, MP_PER_LVL, MP_REGEN_PER_LVL);
