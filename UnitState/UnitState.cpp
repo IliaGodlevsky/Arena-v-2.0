edb38d2a500036c43a9b05309ac658414252d2dd
IliaGodlevsky <godleevsky2015@gmail.com>
2020-01-18 20:03:00 +0300

    Update UnitState.cpp

--- UnitState/UnitState.cpp
+++ UnitState/UnitState.cpp
@@ -4,21 +4,18 @@
 #include "../Magic/Magic.h"
 #include "../Arena/Arena.h"
 #include "../Interface/Interface.h"
-
-UnitState::UnitState(DecisionPtr decision)
-	:m_decision(decision)
-{
-
-}
+#include "InnerUnitState/NotEnoughManaUnitState.h"
+#include "../UnitState/InnerUnitState/DeadUnitState.h"
 
 bool UnitState::castMagic(Unit& caster, Unit& unit, MagicPtr& magic)
 {
 	IManaCost* manaCost = DYNAMIC(IManaCost*, magic);
-	if (!caster.isEnoughManaFor(magic)
-		|| nullptr == magic)
+	if (!caster.isEnoughManaFor(magic)|| nullptr == magic)
 		return false;
 	unit.takeMagicEffect(caster, magic);
 	caster.payMana(manaCost->getCost());
+	if (!caster.m_magicBook.canCastAnySpell())
+		caster.m_stateHolder.takeNew(StatePtr(new NotEnoughManaUnitState(&caster)));
 	return true;
 }
 
@@ -27,31 +24,27 @@ bool UnitState::injureUnit(WeaponPtr& weapon, Unit& unit, int damage)
 	if (nullptr == weapon)
 		return false;
 	weapon->injureUnit(unit, damage);
+	if (!unit.isAlive())
+		unit.m_stateHolder.takeNew(StatePtr(new DeadUnitState(&unit)));
 	return true;
 }
 
-UnitPtr UnitState::chooseUnitToAttack(const Unit& decidingUnit, 
+UnitPtr UnitState::chooseUnitToAttack(DecisionPtr decision, const Unit& decidingUnit, 
 	const Gladiators& units)const
 {
 	return nullptr;
 }
 
-MagicPtr UnitState::chooseMagicToCast(const Unit& decidingUnit, 
+MagicPtr UnitState::chooseMagicToCast(DecisionPtr decision, const Unit& decidingUnit,
 	const Gladiators& units)const
 {
 	return nullptr;
 }
 
-UnitPtr UnitState::chooseUnitToCast(const Unit& decidingUnit,
+UnitPtr UnitState::chooseUnitToCast(DecisionPtr decision, const Unit& decidingUnit,
 	const MagicPtr& magicToCast, const Gladiators& units)const
 {
 	return nullptr;
-}
-
-void UnitState::setDecision(DecisionPtr decision)
-{
-	if (nullptr != decision)
-		this->m_decision = decision;
 }
 
 bool UnitState::operator < (const UnitState& unitState)const
