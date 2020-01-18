38e04143478ca3078a8d4482a0a846d0a733c8bb
IliaGodlevsky <godleevsky2015@gmail.com>
2020-01-18 20:00:20 +0300

    Update Factory.h

--- Factories/Factory.h
+++ Factories/Factory.h
@@ -40,8 +40,7 @@ template <class T>
 std::unique_ptr<T> Factory<T>::createItem()const
 {
 	if (m_items.empty())
-		throw EmptyContainerException("\nTry to fill the container. "
-			"Bad container is " + std::string(typeid(*this).name()));
+		throw EmptyContainerException("\nContainer " + std::string(typeid(*this).name()) + "is empty");
 	index itemIndex = randomNumber(m_items.size() - 1);
 	return std::unique_ptr<T>(m_items[itemIndex]->clone());
 }
