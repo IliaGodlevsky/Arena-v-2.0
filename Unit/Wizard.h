3d09c7017b2dc5838f3a3d4b0ef7c32d89ed4139
IliaGodlevsky <godleevsky2015@gmail.com>
2020-01-18 20:02:12 +0300

    Update Wizard.h

--- Unit/Wizard.h
+++ Unit/Wizard.h
@@ -19,7 +19,6 @@ public:
 	void payMana(int manaCost)override;
 	bool castMagic(Unit& unit, MagicPtr& magic);
 	bool isEnoughManaFor(const MagicPtr& magic)const;
-	UnitPtr getPureClone()const override;
 	~Wizard() = default;
 private:
 	int countManaCost(int manaCost)const;
