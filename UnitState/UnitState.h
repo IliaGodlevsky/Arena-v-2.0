f804f405f0a48b22c8da256e3d22d7d9fff528e5
IliaGodlevsky <godleevsky2015@gmail.com>
2020-01-18 20:03:09 +0300

    Update UnitState.h

--- UnitState/UnitState.h
+++ UnitState/UnitState.h
@@ -12,26 +12,22 @@ class UnitState
 {
 public:
 	UnitState() = default;
-	UnitState(DecisionPtr decision);
 	virtual bool castMagic(Unit& caster, Unit& unit, MagicPtr& magic);
 	virtual bool injureUnit(WeaponPtr& weapon, Unit& unit, int damage);
-	virtual UnitPtr chooseUnitToAttack(const Unit& decidingUnit, 
+	virtual UnitPtr chooseUnitToAttack(DecisionPtr decision, const Unit& decidingUnit, 
 		const Gladiators& units)const = 0;
-	virtual MagicPtr chooseMagicToCast(const Unit& decidingUnit, 
+	virtual MagicPtr chooseMagicToCast(DecisionPtr decision,const Unit& decidingUnit,
 		const Gladiators& units)const = 0;
-	virtual UnitPtr chooseUnitToCast(const Unit& decidingUnit,
+	virtual UnitPtr chooseUnitToCast(DecisionPtr decision, const Unit& decidingUnit,
 		const MagicPtr& magicToCast, const Gladiators& units)const = 0;
 	virtual ~UnitState() = default;
-public:
-	virtual void setDecision(DecisionPtr decision) final;
 	virtual void showShortInfo()const;
 public:
 	virtual bool operator <(const UnitState& unitState)const final;
 	virtual bool operator >(const UnitState& unitState)const final;
 	virtual bool isEqual(const StatePtr& unitState)const final;
 	virtual bool isExpired()const = 0;
-protected:
-	DecisionPtr m_decision = nullptr;
+	virtual StatePtr clone()const = 0;
 private:
 	virtual int getValue()const = 0;
 
