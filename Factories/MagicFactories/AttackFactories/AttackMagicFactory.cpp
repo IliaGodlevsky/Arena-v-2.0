914f24fb20a0dfc2bd94777ff31fd87784e790ee
IliaGodlevsky <godleevsky2015@gmail.com>
2020-01-25 11:29:42 +0300

    added new magic class to factory

--- Factories/MagicFactories/AttackFactories/AttackMagicFactory.cpp
+++ Factories/MagicFactories/AttackFactories/AttackMagicFactory.cpp
@@ -1,4 +1,5 @@
 #include "../../../Magic/AttackMagic/AttackMagic.h"
+#include "../../../Magic/AttackMagic/AttackAndSilenceMagic.h"
 
 #include "AttackMagicFactory.h"
 
@@ -8,4 +9,8 @@ AttackMagicFactory::AttackMagicFactory()
 		MAGIC_ARROW_MANA_COST, MAGIC_ARROW_DAMAGE)));
 	m_items.push_back(MagicPtr(new AttackMagic("Magic hummer", 
 		MAGIC_HUMMER_MANA_COST, MAGIC_HUMMER_DAMAGE)));
+	m_items.push_back(MagicPtr(new AttackAndSilenceMagic("Silence fist", 
+		SILENCE_FIST_MANA_COST, Timer(SILENCE_FIST_DURATION), SILENCE_FIST_DAMAGE)));
+	m_items.push_back(MagicPtr(new AttackAndSilenceMagic("Fear", 
+		FEAR_MANA_COST, Timer(FEAR_DURATION), FEAR_DAMAGE)));
 }
